1d01340bc019e2ed8db609e6e4588b9f3079aaf4
Aleix <algafix@gmail.com>
2020-02-11 00:03:21 +0100

    Eliminar informació redundant

--- scripts/delivery.sh
+++
@@ -1,7 +0,0 @@
-echo "Scenario Created Delivered"
-for scen in Info5 Cambridge Taxis Demo Festival
-do
-created=`grep created reports/default/$scen*MessageStatsReportPerApplication.txt |head -1|cut -d" " -f2`
-delivered=`cat reports/default/$scen*MessageStatsReportPerApplication.txt |grep -A8 "r-r-r-influ"|grep delivered|cut -d" " -f2`
-echo "$scen $created $delivered"
-done
